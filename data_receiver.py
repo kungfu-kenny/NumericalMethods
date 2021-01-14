25ca5c4a8b39a2ed3d31a144ff581060d7010d45
Olexander S <OlexanderS@magetic.com>
2021-01-14 13:23:55 +0200

    Initial Commit

---
+++ data_receiver.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class DataReceiver:
+    """
+    
+    """
+    def __init__(self):
+        self.one = 1
+
+    @staticmethod
+    def produce_values_array():
+        return np.array([[], [], []], dtype=float)
